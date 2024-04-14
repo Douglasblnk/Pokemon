ac34ebee58f4efbe380b1c0b55660dcd9e00df7a
Douglas Blank <douglasblank1@gmail.com>
2024-04-14 16:12:40 -0300

    feat: add quasar dependency and its configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,28 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@unocss/nuxt',
+    'nuxt-quasar-ui',
     '@nuxtjs/eslint-module',
   ],
   srcDir: './src',
   serverDir: './server',
+  quasar: {
+    iconSet: 'mdi-v6',
+    lang: 'pt-BR',
+    config: {
+      brand: {
+        'primary': 'var(--color-primary)',
+        'secondary': 'var(--color-secondary)',
+        'accent': 'var(--color-accent)',
+
+        'dark': 'var(--color-background)',
+        'dark-page': 'var(--color-background)',
+
+        'positive': 'var(--color-positive)',
+        'negative': 'var(--color-negative)',
+        'info': 'var(--color-info)',
+        'warning': 'var(--color-warning)',
+      },
+    },
+  },
 })
