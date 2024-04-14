8777626f8571b81d65589cb67c95486fae879c2c
Douglas Blank <douglasblank1@gmail.com>
2024-04-14 15:17:36 -0300

    feat: add unocss module to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module'],
+  modules: [
+    '@unocss/nuxt',
+    '@nuxtjs/eslint-module',
+  ],
+  srcDir: './src',
+  serverDir: './server',
 })
