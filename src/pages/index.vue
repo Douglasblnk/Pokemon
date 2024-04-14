7cb9d18b1f823dfc9e0d4cc31c24860732e5697c
Douglas Blank <douglasblank1@gmail.com>
2024-04-14 16:44:44 -0300

    feat: implement tanstack data fetch to index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import { getPokemons } from '~/datasource/pokemons'
+
+const search = ref()
+
+const { data } = useInfiniteQuery({
+  queryKey: [ 'pokemons' ],
+  initialPageParam: 'https://pokeapi.co/api/v2/pokemon',
+  queryFn: ({ pageParam }) => getPokemons(pageParam),
+  getNextPageParam: value => value.next,
+  select: data => data.pages.flatMap(page => page.results),
+})
 </script>
 
 <template>
