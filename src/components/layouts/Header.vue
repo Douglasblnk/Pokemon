a30b95eb71119ec4ffe2d23975aa24dbf078fa76
Douglas Blank <douglasblank1@gmail.com>
2024-04-14 16:26:12 -0300

    feat: implement Header component

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <QCard
+    un-rounded-none
+    un-shadow
+  >
+    <QCardSection
+      un-flex
+      un-justify="sm:start center"
+      un-items-center
+      un-gap-sm
+    >
+      <QIcon
+        name="i-custom-kotas-logo"
+        size="sm"
+      />
+
+      <QIcon
+        name="i-custom-kotas"
+        size="xs"
+        un-w-50px
+      />
+    </QCardSection>
+  </QCard>
+</template>
