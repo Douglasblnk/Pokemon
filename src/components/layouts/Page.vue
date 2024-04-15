73b6a055eadf945129116a2a3e98b2d27318e6c9
Douglas Blank <douglasblank1@gmail.com>
2024-04-14 23:42:50 -0300

    feat: add inheritAttrs false to page component

--- src/components/layouts/Page.vue
+++ src/components/layouts/Page.vue
@@ -1,12 +1,20 @@
+<script setup lang="ts">
+defineOptions({ inheritAttrs: false })
+</script>
+
 <template>
   <div
+    id="page-container"
     un-m="sm:lg md"
     un-column
     un-items-center
     un-overflow-auto
     style="height: calc(100vh - 110px);"
   >
-    <div un-w="sm:col-7 col-12">
+    <div
+      v-bind="$attrs"
+      un-w="sm:col-7 col-12"
+    >
       <slot />
     </div>
   </div>
